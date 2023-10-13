8477fd24fbc9c625936eeaa7df9763c990fe811e
sajjad <sadehdev@gmail.com>
2023-10-13 12:36:40 +0330

    remove some useless plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
-  supabase: {
-    redirect: false,
-  },
+  modules: ["@nuxtjs/tailwindcss"],
+
   devtools: { enabled: true },
 });
